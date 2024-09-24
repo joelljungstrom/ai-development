dc19afd69c2afccf321f1dcd86d6933045ae2933
Joel Ljungström <ljungstrom.joel@gmail.com>
2024-09-24 19:32:54 +0200

    Add laboration exercises

---
+++ Laboration/memory.py
@@ -0,0 +1,7 @@
+'''
+Skapa en version av spelet Memory.
+·         Datorn väljer ett antal slumpmässiga siffror eller bokstäver (beroende på svårighetsgrad) och visar dem i en viss ordning.
+·         Sedan visar datorn samma siffror eller bokstäver igen, men denna gång blandat.
+·         Spelaren ska gissa i vilken ordning siffrorna eller bokstäverna visades första gången.
+·         Spelet fortsätter tills spelaren har gissat rätt ordning.
+'''
